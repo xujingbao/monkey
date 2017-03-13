6a49a09ebfbe5190f707a8fe1f5bdd016a71280b
xujingbao <xujingbao@gmail.com>
2017-03-13 21:40:12 +0800

    update

--- admin/src/pages/charts/echarts.vue
+++ admin/src/pages/charts/echarts.vue
@@ -41,7 +41,9 @@
             this.chartPie = echarts.init(document.getElementById('chartPie'));
 
             this.chartColumn.setOption({
-                title: { text: 'Column Chart' },
+                title: {
+                    text: 'Column Chart'
+                },
                 tooltip: {},
                 xAxis: {
                     data: ["衬衫", "羊毛衫", "雪纺衫", "裤子", "高跟鞋", "袜子"]
@@ -82,8 +84,7 @@
                     type: 'category',
                     data: ['巴西', '印尼', '美国', '印度', '中国', '世界人口(万)']
                 },
-                series: [
-                    {
+                series: [{
                         name: '2011年',
                         type: 'bar',
                         data: [18203, 23489, 29034, 104970, 131744, 630230]
@@ -120,8 +121,7 @@
                 yAxis: {
                     type: 'value'
                 },
-                series: [
-                    {
+                series: [{
                         name: '邮件营销',
                         type: 'line',
                         stack: '总量',
@@ -157,33 +157,44 @@
                     left: 'left',
                     data: ['直接访问', '邮件营销', '联盟广告', '视频广告', '搜索引擎']
                 },
-                series: [
-                    {
-                        name: '访问来源',
-                        type: 'pie',
-                        radius: '55%',
-                        center: ['50%', '60%'],
-                        data: [
-                            { value: 335, name: '直接访问' },
-                            { value: 310, name: '邮件营销' },
-                            { value: 234, name: '联盟广告' },
-                            { value: 135, name: '视频广告' },
-                            { value: 1548, name: '搜索引擎' }
-                        ],
-                        itemStyle: {
-                            emphasis: {
-                                shadowBlur: 10,
-                                shadowOffsetX: 0,
-                                shadowColor: 'rgba(0, 0, 0, 0.5)'
-                            }
+                series: [{
+                    name: '访问来源',
+                    type: 'pie',
+                    radius: '55%',
+                    center: ['50%', '60%'],
+                    data: [{
+                            value: 335,
+                            name: '直接访问'
+                        },
+                        {
+                            value: 310,
+                            name: '邮件营销'
+                        },
+                        {
+                            value: 234,
+                            name: '联盟广告'
+                        },
+                        {
+                            value: 135,
+                            name: '视频广告'
+                        },
+                        {
+                            value: 1548,
+                            name: '搜索引擎'
+                        }
+                    ],
+                    itemStyle: {
+                        emphasis: {
+                            shadowBlur: 10,
+                            shadowOffsetX: 0,
+                            shadowColor: 'rgba(0, 0, 0, 0.5)'
                         }
                     }
-                ]
+                }]
             });
         }
 
     }
-
 </script>
 
 <style scoped>
