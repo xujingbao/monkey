c4dfc8cc27a2acb8c8e22b356d4788115ab2d377
xujingbao <xujingbao@gmail.com>
2017-03-15 16:17:17 +0800

    update

--- admin/src/api/api.js
+++ admin/src/api/api.js
@@ -1,17 +1,54 @@
+/**
+ *  Promise based HTTP client for the browser and node.js (https://github.com/mzabriskie/axios)
+ -   Make XMLHttpRequests from the browser
+ -   Make http requests from node.js
+ -   Supports the Promise API
+ -   Intercept request and response
+ -   Transform request and response data
+ -   Cancel requests
+ -   Automatic transforms for JSON data
+ -   Client side support for protecting against XSRF
+ */
 import axios from 'axios';
 
 let base = '';
 
-export const requestLogin = params => { return axios.post(`${base}/login`, params).then(res => res.data); };
+export const requestLogin = params => {
+    return axios.post(`${base}/login`, params).then(res => res.data);
+};
 
-export const getUserList = params => { return axios.get(`${base}/user/list`, { params: params }); };
+export const getUserList = params => {
+    return axios.get(`${base}/user/list`, {
+        params: params
+    });
+};
 
-export const getUserListPage = params => { return axios.get(`${base}/user/listpage`, { params: params }); };
+export const getUserListPage = params => {
+    return axios.get(`${base}/user/listpage`, {
+        params: params
+    });
+};
 
-export const removeUser = params => { return axios.get(`${base}/user/remove`, { params: params }); };
+export const removeUser = params => {
+    return axios.get(`${base}/user/remove`, {
+        params: params
+    });
+};
 
-export const batchRemoveUser = params => { return axios.get(`${base}/user/batchremove`, { params: params }); };
+export const batchRemoveUser = params => {
+    return axios.get(`${base}/user/batchremove`, {
+        params: params
+    });
+};
 
-export const editUser = params => { return axios.get(`${base}/user/edit`, { params: params }); };
+export const editUser = params => {
+    return axios.get(`${base}/user/edit`, {
+        params: params
+    });
+};
 
-export const addUser = params => { return axios.get(`${base}/user/add`, { params: params }); };
+export const addUser = params => {
+    return axios.get(`${base}/user/add`, {
+        params: params
+    });
+};
